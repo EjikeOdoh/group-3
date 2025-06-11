220077e80aa3312eae0c6cc59235ce5e425c25bb
EjikeOdoh <ejike.developer@gmail.com>
2025-06-11 17:46:40 +0100

    Complete settings page

--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import styles from '../styles/quizzes.module.css'
 import Header from '../component/Header'
 
+import Code from '../thumbnail/code.png'
+import Algebra from '../thumbnail/algebra.png'
+import Cell from '../thumbnail/cell.png'
+import Science from '../thumbnail/science.png'
+import Figma from '../thumbnail/figma.png'
+
+
+function Quiz(props) {
+    return (
+        <div className={styles.quiz}>
+            <div className={styles.left}>
+                <div className={styles.icon}>
+                    <img src={props.icon} />
+                </div>
+                <h4>{props.course}</h4>
+            </div>
+            <button>Start</button>
+        </div>
+    )
+}
+
 export default function Quizzes() {
-  return (
-    <div>
-      <Header 
-label="Quiz"
-      />
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Header
+                label="Quiz"
+            />
+            <div className={styles.top}></div>
+            <div className={styles.content}>
+                <Quiz
+                    course="Coding and Design"
+                    icon={Code}
+                />
+                <Quiz
+                    course="Algebra Quiz"
+                    icon={Algebra}
+                />
+                <Quiz
+                    course="Science Quiz"
+                    icon={Science}
+                />
+                <Quiz
+                    course="Biology Quiz"
+                    icon={Cell}
+                />
+                <Quiz
+                    course="Figma Quiz"
+                    icon={Figma}
+                />
+            </div>
+        </div>
+    )
 }
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import styles from '../styles/setting.module.css'
 import Header from '../component/Header'
+import Avatar from '../images/avatar.jpeg'
+import WhiteButton from '../component/WhiteButton'
 
 export default function Settings() {
   return (
-    <div>
+    <div className={styles.container}>
       <Header
-      label="Profile"
+        label="Profile"
       />
+      <div className={styles.top}></div>
+      <div className={styles.content}>
+        <div className={styles.top}>
+          <div className={styles.pageInfo}>
+            <h2>
+              Profile Information
+            </h2>
+            <small>Manage your profile details and log in credentials</small>
+          </div>
+          <div className={styles.photo}>
+            <img src={Avatar} />
+          </div>
+
+          <div className={styles.pageAction}>
+            <div className={styles.btns}>
+              <WhiteButton text="Replace" />
+              <WhiteButton text="Remove" />
+            </div>
+            <small>Image sizes should be at 320x big, and less than 500kb.Allowed files, png and jpg.</small>
+          </div>
+        </div>
+
+        <form className={styles.myForm}>
+          <div className={styles.cat}>
+            <label htmlFor="">First name</label>
+            <input type="text" placeholder="Edit first name" />
+          </div>
+
+          <div className={styles.cat}>
+            <label htmlFor="">Last name</label>
+            <input type="text" placeholder="Edit last name" />
+          </div>
+        </form>
+
+      </div>
     </div>
   )
 }
